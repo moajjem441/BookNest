8d537e274c430bfb885acf032aaa352687205576
Moajjem Hossain <moajjem441@gmail.com>
2026-07-23 03:27:25 +0600

    featuredBooks part added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
+import FeaturedBooks from "./Components/FeaturedBooks";
 import Hero from "./Components/Hero";
 
 
 export default function Home() {
   return (
    <div>
+
    <Hero/>
+
+   <FeaturedBooks></FeaturedBooks>
+   
    </div>
   );
 }
