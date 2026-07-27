2471a10e93716ec346477c2b799a5fa677a8ce35
Moajjem Hossain <moajjem441@gmail.com>
2026-07-27 23:53:41 +0600

    login part updated

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -40,6 +40,7 @@ export default function LoginPage() {
           password: formData.password,
           callbackURL: "/",
         },
+        
         {
           onRequest: () => {
             setIsLoading(true);
